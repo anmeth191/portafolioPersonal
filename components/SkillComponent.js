7c9efa80aeb39e34cfb19434e7e74e8a527236ee
Angel Rivera <38502560+anmeth191@users.noreply.github.com>
2021-05-26 22:37:16 -0400

    Add files via upload
    today i worked in the skills section using react to control de progress bars and used an animation to give the effect in the bars to load , pretty cool !!

--- components/SkillComponent.js
+++ components/SkillComponent.js
@@ -1,12 +1,69 @@
 import React from 'react';
+import Navbar from './Navbar';
+import { connect } from 'react-redux';
+class  SkillComponent  extends React.Component{
 
-const SkillComponent = ()=>{
+    state={
+        widthBar: 0
+    }
 
-return(
-    <div>Hello from Skill Component
-        </div>
-)
+seeAnimation = ()=>{
 
+    const animation = setInterval(()=>{
+        alert('Hello world')
+    } , 2000)
 }
 
-export default SkillComponent;
+render(){
+    const { skills } = this.props;
+    
+    
+return(
+    <div>
+        <Navbar />
+        <div className="skillMain">
+           <div className="skillMain-container">
+            {
+              skills.map(skill =>{
+                  let barWidth = 0;
+                   if(skill === 'JavaScript'){ barWidth = 82}
+                   if(skill === 'HTML'){barWidth = 70 }
+                   if(skill === 'CSS'){barWidth = 60}
+                   if(skill === 'React'){barWidth = 50}
+                   if(skill === 'NodeJS'){barWidth = 48}
+                   if(skill === 'MongoDB'){barWidth = 47}
+                   if(skill === 'Angular'){barWidth = 50}
+                   if(skill === 'MYSQL'){barWidth = 55}
+                   if(skill === 'Team Work'){barWidth = 95}
+                   if(skill === 'Communication Skills'){barWidth = 90}
+                   if(skill === 'Problem Solving'){barWidth = 92}
+                   if(skill === 'Customer Service'){barWidth = 91}
+                   if(skill === 'Glovia'){barWidth = 82}
+                   if(skill === 'Mainframe'){barWidth = 80}
+                return(
+                      <div key={skill} className="skillMain-container-skill">
+                          <h3 className="skillMain-container-skill-title">{skill}</h3>
+                          <div className="skillMain-container-skill-progressBar">
+                          <div className="skillMain-container-skill-progressBar-width" style={{ width:`${barWidth}%`}}>
+                          <span className="skillMain-container-skill-progressBar-width-span">{`${barWidth}%`}</span>
+                            </div>
+                          </div>
+                    </div>
+                  )
+              })
+            }
+           </div>
+            </div>
+
+        </div>
+)//end of the return
+}//end of the render
+}//end of the class Component
+
+const setSkillsProps = (state)=>{
+
+    return{
+        skills:state.skills
+    }
+}
+export default connect(setSkillsProps)(SkillComponent);
--- reducers/personalInfo.js
+++ reducers/personalInfo.js
@@ -1,17 +1,18 @@
 const personalData = {
     
-    name:'Christina',
+    name:'Angel',
     lastName:'Rivera',
     address:'115 Beaver Road',
     city:'Maven',
     state:'PA',
     zipCode:32186,
+    skills:['JavaScript' , 'HTML' , 'CSS' , 'React' , 'NodeJS' , 'MongoDB' , 'Angular' , 'MYSQL' , 'Team Work',
+            'Communication Skills' , 'Problem Solving' , 'Customer Service' , 'Glovia' , 'Mainframe' ]
 }
 
 
 const personalInfo = (state = personalData , action)=>{
-
-    return state;
+return state;
 }
 
 export default personalInfo;
