00ce5949c41c5901c737fa8ffc4634e36703ff11
Angel Rivera <38502560+anmeth191@users.noreply.github.com>
2021-06-01 21:56:50 -0400

    Add files via upload
    finished the responsive design for phones , i have been brushing up more CSS with grid and Sass

--- components/EducationComponent.js
+++ components/EducationComponent.js
@@ -13,10 +13,12 @@ return(
          <div className="education-container">
              <div className="education-container-centered">
                  <div className="education-container-centered-content">
+                 <div className="education-container-centered-content-center">   
              <div className="education-container-centered-content-title"> <h1 className="education-container-centered-content-title-tag"> {education.title} </h1></div>
-             <div className="education-container-centered-content-concentration"> <h2 className="education-container-centered-content-concentration-tag">{education.concentration}</h2></div>
-             <div className="education-container-centered-content-university"> <h2 className="education-container-centered-content-university-tag">{education.university}</h2></div>
-             <div className="education-container-centered-content-description"><h2 className="education-container-centered-content-description-tag"> {education.description}</h2></div>
+             <div className="education-container-centered-content-concentration"> <h3 className="education-container-centered-content-concentration-tag">{education.concentration}</h3></div>
+             <div className="education-container-centered-content-university"> <h3 className="education-container-centered-content-university-tag">{education.university}</h3></div>
+             <div className="education-container-centered-content-description"><h3 className="education-container-centered-content-description-tag"> {education.description}</h3></div>
+             </div>
              </div>
              <div className="education-container-centered-image">
                  </div>
--- components/ExperienceComponent.js
+++ components/ExperienceComponent.js
@@ -13,19 +13,24 @@ class ExperienceComponent extends React.Component{
               <Navbar />
               <div className="experience">
                   <div className="experience-container">
+                    
               {jobs.map(element =>{
                   return(
+                      
                       <div className={`experience-container-element${element.id}`} key={element.id} >
+                          <div className="experience-container-center">
                           <h1 className="experience-container-title">{element.title}</h1>
                           <h3 className="experience-container-description">{element.description}</h3>
                           <h5 className="experience-container-date">{element.date}</h5>
                           <h5 className="experience-container-location">{element.location}</h5>
                           </div>
+                          </div>
                         )//end of the return
               })//end of the map
             } </div>
+            </div>
               </div>
-          </div>
+          
       )
     }
 }
